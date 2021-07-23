37d57c9bf0e41416282d440a639cbf70376d1434
Shin13 <soobahorn@gmail.com>
2021-07-23 11:48:54 +0800

    Update SUMMARY.md & processing .py files

--- modify-auto-summary.py
+++ modify-auto-summary.py
@@ -8,23 +8,24 @@ with open('SUMMARY.md', 'rb') as f:
     contents = contents.replace('\\', '/')
     contents = contents.replace('./', '')
 
-cat_list = pd.read_excel('cat_ref.xlsx')
-cat_list['code'] = '[' + cat_list['code'].astype(str) + ']'
-cat_list['name'] = '[' + cat_list['name'].astype(str) + ']'
+# cat_list = pd.read_excel('cat_ref.xlsx')
+# cat_list['code'] = '[' + cat_list['code'].astype(str) + ']'
+# cat_list['name'] = '[' + cat_list['name'].astype(str) + ']'
 
-rep = dict(zip(cat_list['code'], cat_list['name']))
-rep = dict((re.escape(k), v) for k, v in rep.items())
-pattern = re.compile("|".join(rep.keys()))
-text = pattern.sub(lambda m: rep[re.escape(m.group(0))], contents)
+# rep = dict(zip(cat_list['code'], cat_list['name']))
+# rep = dict((re.escape(k), v) for k, v in rep.items())
+# pattern = re.compile("|".join(rep.keys()))
+# text = pattern.sub(lambda m: rep[re.escape(m.group(0))], contents)
 
 rep = dict({'[Readme]': '[首頁]', '[Index]': '[索引]',
             '[Changelog]': '[更新歷程]', '[Appendix]': '[附錄]', '[Links]': '[常用連結]'})
 rep = dict((re.escape(k), v) for k, v in rep.items())
 pattern = re.compile("|".join(rep.keys()))
-text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
+text = pattern.sub(lambda m: rep[re.escape(m.group(0))], contents)
 
-with open('SUMMARY.md', 'w') as f:
+# Remove blank lines
+text = re.sub(r'\n\s*\n', '\n', text)
+with open('SUMMARY.md', 'w+', encoding='utf-8') as f:
     f.write(text)
 
-print('SUMMARY.md 自訂修改完成 :) ')
-
+print('SUMMARY.md 自訂修改完成 :)')
